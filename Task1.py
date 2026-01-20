3a6867b1788a30880fe1bd0c2cfc8a1b8600744b
hruday578 <hrudayreddy310@gmail.com>
2026-01-20 19:55:56 +0530

    your message

---
+++ Task1.py
@@ -0,0 +1,12 @@
+a=float(input("enter first number"))
+b=float(input("enter second number"))
+
+add=a+b
+sub=a-b
+div=a/b
+mul=a*b
+
+print("addition:",add)
+print("subtraction:",sub)
+print("division:",div)
+print("multiplication:",mul)
---
+++ Task2.py
@@ -0,0 +1,5 @@
+first_name=input("Enter your first name: ")
+last_name=input("Enter your last name: ")
+
+print("hello, "+first_name,last_name+"! welcome to python programming")
+
